6a31d77aa1c83738cd64610f704d65ec39a71569
sara <sara722.s@gmail.com>
2020-05-10 15:47:14 +0430

    Update train.py

--- train.py
+++ train.py
@@ -147,7 +147,7 @@ model.compile(loss='binary_crossentropy',
 # model.summary()
 
 #path for saving checpoints:
-filepath="./check_points/" + num_classes + 'class_',model_name + "-{epoch:02d}-{loss:.4f}.h5"
+filepath="./check_points/" + num_classes + 'class_' + model_name + "-{epoch:02d}-{loss:.4f}.h5"
 # filepath="./check_points/" + model_name + "-{epoch:02d}-{loss:.4f}.h5"
 #saving the checkpoints:
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
